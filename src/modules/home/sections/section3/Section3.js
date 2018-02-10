78bcb502af8daec265994868e5460912692d10c4
nevernervous <nevernervous@email.com>
2018-02-10 02:19:42 +0200

    content changes

--- src/modules/home/sections/section3/Section3.js
+++ src/modules/home/sections/section3/Section3.js
@@ -24,22 +24,7 @@ const Section3 = () => (
         <Grid.Row centered>
             <Grid.Column width={10}>
                 <p className='section3-subtitle'>At ClassPorch, we take ample time to interview all our tutors to ensure they
-                 are well qualified before they are granted a tutoring position. <br />
-<b>24-7 Availability</b>
-<br />
-Our tutors are available 24-7 enabling students to reach out for help at their convenience.
-<br />
-<b>Variety of Subjects</b>
-<br />
-We offer tutoring services for a wide variety of subjects ranging from K-5, middle & high school and college subjects. 
-<br />
-<b>Homework made Easy</b>
-<br />
-Our tutors are qualified and readily available to help you with homework and assignments.
-<br />
-<b>Whiteboard + Audio/Video Compatibility</b>
-<br />
-Our highly interactive technology makes the learning process more effective and fun for both tutors and students.</p>
+                 are well qualified before they are granted a tutoring position.</p>
             </Grid.Column>
         </Grid.Row>
         <Grid.Row className='section-three-features'>
@@ -50,13 +35,8 @@ Our highly interactive technology makes the learning process more effective and 
                             <Grid.Row textAlign='right'>
                                 <Grid.Column width={12}>
                                     <p className='section-three-feature-header'>24-7 Availability</p>
-                                    <p className='section-three-feature-details'>Our tutors at ClassPorch are always
-                                        available 24/7 at anytime
-                                        and any day for the student. This will enable the student ask for help at their
-                                        own convenience whenever
-                                        they choose. It is a great thing that with our online tutor, college students
-                                        can now learn 24/7 with an
-                                        online instructor making contact through live chat or online email.</p>
+                                    <p className='section-three-feature-details'>Our tutors are available 24-7 enabling students
+                                     to reach out for help at their convenience.</p>
                                 </Grid.Column>
                                 <Grid.Column width={4}>
                                     <Image style={{paddingTop: 20}} src={imageOne}/>
@@ -72,16 +52,8 @@ Our highly interactive technology makes the learning process more effective and 
                                 </Grid.Column>
                                 <Grid.Column width={12}>
                                     <p className='section-three-feature-header'>Variety of Subjects</p>
-                                    <p className='section-three-feature-details'>At ClassPorch, we offer assistance in a
-                                        large variety of
-                                        subjects ranging from K-5, Middle School, High School, College and University.
-                                        Our tutors will help with
-                                        your school homework or college/university assignments, provide timely test prep
-                                        tips and offer expert
-                                        advice with your AP courses. If you happen to need help with college math for
-                                        example, just search for
-                                        college math help and you will find our online tutors ready to offer their
-                                        services.</p>
+                                    <p className='section-three-feature-details'>We offer tutoring services 
+                                    for a wide variety of subjects ranging from K-5, middle & high school and college subjects. </p>
                                 </Grid.Column>
                             </Grid.Row>
                         </Grid>
@@ -93,15 +65,8 @@ Our highly interactive technology makes the learning process more effective and 
                             <Grid.Row textAlign='right'>
                                 <Grid.Column width={12}>
                                     <p className='section-three-feature-header'>Homeworks made easy</p>
-                                    <p className='section-three-feature-details'>Irrespective the problem or question
-                                        you need help with, our
-                                        experienced tutors will help you work it out. We do not simply provide our
-                                        students with ready-to-go
-                                        answers but make sure they understand the solution and can easily work out
-                                        similar tasks on their own.
-                                        Homework help with online tutoring can give a student the needed support to
-                                        succeed and regain the
-                                        confidence to become a better learner.</p>
+                                    <p className='section-three-feature-details'>Our tutors are qualified and readily available to
+                                     help you with homework and assignments.</p>
                                 </Grid.Column>
                                 <Grid.Column width={4}>
                                     <Image style={{paddingTop: 20}} src={imageThree}/>
@@ -118,12 +83,8 @@ Our highly interactive technology makes the learning process more effective and 
                                 <Grid.Column width={12}>
                                     <p className='section-three-feature-header'> Whiteboard + Audio/Video
                                         Compatability</p>
-                                    <p className='section-three-feature-details'>With our interactive technology at
-                                        ClassPorch, we make
-                                        learning very effective and fun. Our virtual classroom is equipped with live
-                                        video, whiteboard, text-
-                                        and voice-chat, and file sharing tools that allow students to get the most out
-                                        of every session.</p>
+                                    <p className='section-three-feature-details'>Our highly interactive technology
+                                     makes the learning process more effective and fun for both tutors and students.</p>
                                 </Grid.Column>
                             </Grid.Row>
                         </Grid>
